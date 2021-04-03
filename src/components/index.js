ee0af32041f13817f83051b99ce9c9d04e570bc7
Richard <richard.d.merced@gmail.com>
2021-04-02 21:28:56 -0700

    nav bar and AllProducts components showing up

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,6 @@
 //COMPONENTS SWITCHBOARD
 //IF YOU CREATE A COMPONENT MAKE SURE TO EXPORT HERE
 
-export { default as App } from './App';
 export { default as NavBar } from "./NavBar";
 export { default as SingleProduct} from './products/SingleProduct';
 export { default as AllProducts } from './products/AllProducts';
