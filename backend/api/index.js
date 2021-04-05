6de728946510a69a62b7c8d0afa9782357d0fd18
Karla Rodriguez <karla0611@gmail.com>
2021-04-04 18:50:55 -0700

    users/me GET created

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const apiRouter = express.Router();
-const productsRouter = require('./products');
 
+
+// api/products
+const productsRouter = require('./products');
 apiRouter.use('/products', productsRouter);
+
+// api/users
+const usersRouter = require('./users')
+apiRouter.use('/users', usersRouter);
+
 
 module.exports = apiRouter;
