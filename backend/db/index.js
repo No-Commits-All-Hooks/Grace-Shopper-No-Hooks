6b49e95459d0fe15b253f66fd72538abceea1554
Cody Kitchel <kittco1992@gmail.com>
2021-04-06 13:39:20 -0700

    Orders Adapter Functions

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,6 +2,6 @@ module.exports = {
   ...require('./client'), // adds key/values from users.js
   ...require('./users'), // adds key/values from users.js
   ...require('./products'), // adds key/values from products.js
-  // ...require('./orders'), // etc
+  ...require('./orders'), // etc
   // ...require('./order_products') // etc
 };
