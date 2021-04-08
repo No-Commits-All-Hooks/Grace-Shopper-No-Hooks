f44c52371602a2b36f1c6a770f1a2e248a5fbd21
Richard <richard.d.merced@gmail.com>
2021-04-08 16:16:31 -0700

    updated api/user.js

--- backend/api/users.js
+++ backend/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 const { requireUser } = require('./utils');
 const { createUser,
   getUserByUsername,
