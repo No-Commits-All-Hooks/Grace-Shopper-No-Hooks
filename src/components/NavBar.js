c4a2b979d10d74357646eee5c50851dc3a52e0b2
Karla Rodriguez <karla0611@gmail.com>
2021-04-07 23:18:21 -0700

    navBar css + navBar routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,113 @@
-import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
-const NavBar= ({}) =>{
 
-    return (
-        <nav className="products-nav">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/products"}>Products</Link>
-            <Link to = {"/login"}>Login</Link>
 
-        </nav>
-    )
-}
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openAnchor = Boolean(anchorEl);
+  const history = useHistory();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuChange = (pageURL) => {
+    history.push(pageURL);
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar 
+      position="fixed" 
+      style={{ background: 'rgb(102, 30, 30)' }}
+      >
+        <Toolbar>
+          <Typography 
+          variant="h6" 
+          className={classes.title} 
+          onClick={() => handleMenuChange("/")}
+          style={{ cursor: 'pointer' }}
+          >
+            Fullstack Academy Shop
+          </Typography>
+          <Button
+            aria-controls="fade-menu"
+            aria-haspopup="true"
+            onClick={() => handleMenuChange("/products")}
+            style={{ color: 'white' }}
+          >
+            Products
+          </Button>
+          <div>
+            <IconButton
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={openAnchor}
+              onClose={() => setAnchorEl(null)}
+            >
+              <MenuItem 
+              onClick={() => handleMenuChange("/login")}
+              >
+                My Account
+              </MenuItem>
+              <MenuItem 
+              onClick={() => handleMenuChange("/register")}
+              >
+                Sign Up
+              </MenuItem>
+              <MenuItem 
+              onClick={() => handleMenuChange("/orders/cart")}
+              >
+                Shopping Cart
+              </MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default NavBar;
