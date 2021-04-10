0d9dcc4f8ed85c2303d21e6eda4e9f6e86bb905b
Karla Rodriguez <karla0611@gmail.com>
2021-04-10 12:09:01 -0700

    created attachProductstoOrders

--- backend/db/seed.js
+++ backend/db/seed.js
@@ -204,20 +204,23 @@ async function testDB(){
     // console.log("getUser Result:", sal); 
 
 
-    const getOrder = await getOrderById(1);
-    console.log("getOrderById Result:", getOrder);
+    // const getOrder = await getOrderById(1);
+    // console.log("getOrderById Result:", getOrder);
 
     // const getProducts = await getAllProducts();
     // console.log("getAllProducts Result:", getProducts);
 
-    const salOrders = await getOrdersByUser({id : 1});
-    console.log("getOrdersByUser Result:", salOrders);
+    //  const getOrders = await getAllOrders();
+    // console.log("getOrders Result:", getOrders);
 
-    const productsOrders= await getOrdersByProduct({ id:1 }) 
-    console.log("productsOrders Result:", productsOrders);
+    // const salOrders = await getOrdersByUser({id : 1});
+    // console.log("getOrdersByUser Result:", salOrders);
 
-    // const salCart= await getCartByUser({id :1}) 
-    // console.log("getCartByUser Result:", salCart);
+    // const productsOrders= await getOrdersByProduct({ id:1 }) 
+    // console.log("productsOrders Result:", productsOrders);
+
+    // const usersCart= await getCartByUser({id :2}) 
+    // console.log("getCartByUser Result:", usersCart);
     
   } catch(error){
     throw error
