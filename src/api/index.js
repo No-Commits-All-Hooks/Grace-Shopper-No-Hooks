47ddd5a2416e6b8b2e3de145986fe7c51075a6c5
Richard <richard.d.merced@gmail.com>
2021-04-15 21:46:57 -0700

    updated api index frontedn to point to online

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,11 @@
 require('dotenv').config();
 //For now I am leaving it just as this, but you can imagine you could use process.env || heroku URL when you all set that up
 
-export const BASE_URL = 'http://localhost:5000/api/';
+export const BASE_URL = process.env.API_URL || 'https://morning-fortress-53362.herokuapp.com/api/';
+
+// 'http://localhost:5000/api/' use this if you want to go local
 
 
-// process.env.API_URL || 'https://morning-fortress-53362.herokuapp.com/api/';
 
 export const callApi = async ({ url, method, token, body }) => {
   console.log('callApi: ', { url, method, token, body });
