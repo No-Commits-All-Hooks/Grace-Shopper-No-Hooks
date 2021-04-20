428d2b8b6adaa7529952557453d71f498292eb1c
Cody Kitchel <kittco1992@gmail.com>
2021-04-19 19:01:30 -0700

    changed heroku url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 //For now I am leaving it just as this, but you can imagine you could use process.env || heroku URL when you all set that up
 
-export const BASE_URL = process.env.API_URL || 'https://morning-fortress-53362.herokuapp.com/api/';
+export const BASE_URL = process.env.API_URL || 'https://fullstack-fans.herokuapp.com/api/';
 
 
 
